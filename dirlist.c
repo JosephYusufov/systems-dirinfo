7c4f43f861b8e85d5d5f86a5cf0b035ee6d67d6c
JosephYusufov <jyusufov00@stuy.edu>
2019-11-17 22:39:09 -0500

    finished assignment

--- dirlist.c
+++ dirlist.c
@@ -7,14 +7,32 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 int main(){
-    DIR * this_dir;
-    this_dir = opendir(".");
-    struct dirent *files = readdir(this_dir);
-    int index = 0;
-    for(;files!= NULL; index++, (files+index) = readdir(this_dir)){
-        printf("%s\n", (files+index)->d_name);
+  printf("**********\n");
+  printf("Joseph Yusufov\n\nThis Directory\n\nDue: 2019-11-18\n");
+  printf("**********\n");
+  DIR * this_dir;
+  this_dir = opendir(".");
+  struct dirent *files = readdir(this_dir);
+  for(;files!= NULL; files = readdir(this_dir)){
+
+    // Make a buffer to hold stat info
+    struct stat statbuff;
+    if(files->d_type == 4){
+      // Print size, and whether or not is dir
+      stat(files->d_name, &statbuff);
+      printf("d-\t%lld", statbuff.st_size);
+      printf(ANSI_COLOR_CYAN); 
+      printf("\t%s/\n", files->d_name);
+      printf(ANSI_COLOR_RESET);
+    } else {
+      stat(files->d_name, &statbuff);
+      printf("--\t%lld", statbuff.st_size); 
+      printf("\t%s\n", files->d_name);
     }
-    return 0;
+  }
+  return 0;
 }
