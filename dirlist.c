0cedbc5cf3d005fc23ef789996011c057cbb2e2b
JosephYusufov <jyusufov00@stuy.edu>
2019-11-18 21:14:39 -0500

    finished dirinfo assignment with command-line-input addendum

--- dirlist.c
+++ dirlist.c
@@ -8,14 +8,21 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_GREEN    "\x1b[32m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-int main(){
+int main(int argc, char *argv[]){
+  char *path = ".";
+  if(argc > 1){
+    path = argv[1];
+  }
+
   printf("**********\n");
-  printf("Joseph Yusufov\n\nThis Directory\n\nDue: 2019-11-18\n");
+  printf("Joseph Yusufov dirinfo Assignment\nDirectory: \"%s\"\nDue: 2019-11-19\n", path);
+  printf(ANSI_COLOR_GREEN "NOTE: " ANSI_COLOR_RESET "Pass a path as an argument to view the info of that directory\n");
   printf("**********\n");
   DIR * this_dir;
-  this_dir = opendir(".");
+  this_dir = opendir(path);
   struct dirent *files = readdir(this_dir);
   for(;files!= NULL; files = readdir(this_dir)){
 
@@ -25,12 +32,12 @@ int main(){
       // Print size, and whether or not is dir
       stat(files->d_name, &statbuff);
       printf("d-\t%lld", statbuff.st_size);
-      printf(ANSI_COLOR_CYAN); 
+      printf(ANSI_COLOR_CYAN);
       printf("\t%s/\n", files->d_name);
       printf(ANSI_COLOR_RESET);
     } else {
       stat(files->d_name, &statbuff);
-      printf("--\t%lld", statbuff.st_size); 
+      printf("--\t%lld", statbuff.st_size);
       printf("\t%s\n", files->d_name);
     }
   }
